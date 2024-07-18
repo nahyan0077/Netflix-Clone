0609a1c2ef738f6d64eb0ceb24f6f0a32de9dc57
nahyan0077 <nahyanmp4@gmail.com>
2024-07-18 16:43:30 +0530

    .env added

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -1,20 +1,18 @@
-
+// src/firebase/Config.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnp4GAc87iSGxFegGoLXvf0hsG3InJfwQ",
-  authDomain: "netflix-clone-74aa8.firebaseapp.com",
-  projectId: "netflix-clone-74aa8",
-  storageBucket: "netflix-clone-74aa8.appspot.com",
-  messagingSenderId: "251455809613",
-  appId: "1:251455809613:web:20884ac640621a3752d404",
-  measurementId: "G-YC8LRK09TL"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
-
-
 
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 
-export default firestore
+export default firestore;
