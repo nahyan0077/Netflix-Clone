19fd1dfe228fdb61dca7d4bfd1f2adc2bd9503b0
nahyan0077 <nahyanmp4@gmail.com>
2024-07-19 15:52:12 +0530

    updated the keys

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDnp4GAc87iSGxFegGoLXvf0hsG3InJfwQ",
+  authDomain: "netflix-clone-74aa8.firebaseapp.com",
+  projectId: "netflix-clone-74aa8",
+  storageBucket: "netflix-clone-74aa8.appspot.com",
+  messagingSenderId: "251455809613",
+  appId: "1:251455809613:web:20884ac640621a3752d404",
+  measurementId: "G-YC8LRK09TL"
 };
 
 const app = initializeApp(firebaseConfig);
